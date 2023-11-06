2817d6b66f189c55889f09894fe523ba6e96be76
SangHyeon <sjyrsh0712@hanyang.ac.kr>
2023-11-06 12:46:16 +0900

    test

---
+++ beClear/beClear/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  beClear
+//
+//  Created by 박상현 on 2023/11/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ beClear/beClear/beClearApp.swift
@@ -0,0 +1,17 @@
+//
+//  beClearApp.swift
+//  beClear
+//
+//  Created by 박상현 on 2023/11/06.
+//
+
+import SwiftUI
+
+@main
+struct beClearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
